4d7fcbbde2c615510ab71875833278eb904582fd
Preetam Sain <preetamsain36@gmail.com>
2026-09-02 01:03:07 +0530

    feat: implement 3-stage hydraulic tipper elevation animation showcase using brochure assets

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -30,6 +30,7 @@ import {
 } from '../data/trailers';
 import { AnimatedCounter } from '../components/AnimatedCounter';
 import { HeroParticles } from '../components/HeroParticles';
+import { TipperElevationShowcase } from '../components/TipperElevationShowcase';
 import { motion, AnimatePresence } from 'framer-motion';
 
 export const HomePage: React.FC = () => {
@@ -225,6 +226,9 @@ export const HomePage: React.FC = () => {
 
         </div>
       </section>
+
+      {/* 2.5. 3-STAGE HYDRAULIC TIPPING ANIMATION SHOWCASE */}
+      <TipperElevationShowcase />
 
       {/* 3. ENGINEERING & METALLURGY EXCELLENCE */}
       <section className="py-16 md:py-24 bg-white border-y border-[#EFE8DF]">

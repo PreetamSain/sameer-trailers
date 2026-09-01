62d2edeca3d356514c283200c002b15aa2533cf0
Preetam Sain <preetamsain36@gmail.com>
2026-09-01 23:32:05 +0530

    feat: add minimal interactive scroll highway progress bar with trailer indicator

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-route
 import Lenis from 'lenis';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { ScrollProgressBar } from './components/ScrollProgressBar';
 import { HomePage } from './pages/HomePage';
 import { ProductsPage } from './pages/ProductsPage';
 import { ProductDetailPage } from './pages/ProductDetailPage';
@@ -49,6 +50,9 @@ export function App() {
       <div className="flex flex-col min-h-screen bg-[#FFFBF7] text-[#3B3A3A]">
         {/* Clean Sticky Header */}
         <Header />
+
+        {/* Minimal Interactive Scroll Highway Progress Bar */}
+        <ScrollProgressBar />
 
         {/* Main Content */}
         <main className="flex-1">
